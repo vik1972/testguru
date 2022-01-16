981ff1e8111d658e0fce7fff19006ffa94eebb75
viktor <bvv33@yandex.ru>
2022-01-16 13:35:10 +0200

    Solved problems 2 from Rails_Basic

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Kaliningrad"
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale= :ru
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
