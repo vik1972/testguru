ee13e6f59ffd82f3fca964c1dc76548ee3de157a
viktor <bvv33@yandex.ru>
2022-01-28 20:32:01 +0200

    Fixed errors in User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :passed_tests
   has_many :tests, through: :passed_tests
-  
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
   def test_by_level(level)
     Test.joins(:passed_tests)
