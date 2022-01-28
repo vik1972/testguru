385eec4fbe1d476872ff6cdeb941b412c3d12438
viktor <bvv33@yandex.ru>
2022-01-28 17:33:34 +0200

    Solution problem 5 from Rails Basic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+  
 
- def test_by_level(level)
-    Test.joins('JOIN passed_tests ON tests.id = passed_tests.test_id')
+  def test_by_level(level)
+    Test.joins(:passed_tests)
         .where('passed_tests.user_id = ? AND tests.level = ?', id, level)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_153120) do
+ActiveRecord::Schema.define(version: 2022_01_27_195911) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_01_25_153120) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2022_01_25_153120) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
